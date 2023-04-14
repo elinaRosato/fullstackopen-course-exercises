09aab51debbad2bb7a8fcba31f86eecca2566f6b
Elina Rosato <rosatoelina@gmail.com>
2023-04-14 10:55:28 +0200

    unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,12 +8,15 @@ const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
   return (
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
@@ -22,19 +25,45 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const updateGood = () => setGood(good + 1)
-  const updateNeutral = () => setNeutral(neutral + 1)
-  const updateBad = () => setBad(bad + 1)
+  const updateFeedback = (value) => {
+    let updatedTotal
+    if (value == 1) {
+      const updatedGood = good + 1
+      updatedTotal = updatedGood + neutral + bad
+      setGood(updatedGood)
+      setAverage((updatedGood-bad)/updatedTotal)
+      setPositive(updatedGood/updatedTotal)
+    } else if (value == 0) {
+      const updatedNeutral = neutral + 1
+      updatedTotal = good + updatedNeutral + bad
+      setNeutral(neutral + 1)
+      setAverage((good-bad)/updatedTotal)
+      setPositive(good/updatedTotal)
+    } else if (value == -1) {
+      const updatedBad = bad + 1
+      updatedTotal = good + neutral + updatedBad
+      setBad(bad + 1)
+      setAverage((good-updatedBad)/updatedTotal)
+      setPositive(good/updatedTotal)
+    }
+    console.log(total)
+    setTotal(updatedTotal)
+    console.log(total)
+    console.log('average: ', average)
+  }
 
   return (
     <div>
       <Heading text="give feedback" />
-      <Button handleClick={updateGood} text="good" />
-      <Button handleClick={updateNeutral} text="neutral" />
-      <Button handleClick={updateBad} text="bad" />
+      <Button handleClick={() => updateFeedback(1)} text="good" />
+      <Button handleClick={() => updateFeedback(0)} text="neutral" />
+      <Button handleClick={() => updateFeedback(-1)} text="bad" />
       <Heading text="statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive} />
     </div>
   )
 }
