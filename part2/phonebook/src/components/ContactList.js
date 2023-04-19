3de7ca36a068292de1aabe882a00e7d173e01b52
Elina Rosato <rosatoelina@gmail.com>
2023-04-19 11:26:05 +0200

    phonebook step6

--- part2/phonebook/src/components/ContactList.js
+++ part2/phonebook/src/components/ContactList.js
@@ -3,7 +3,9 @@ import Contact from './Contact'
 const ContactList =  ({ personsToShow }) => {
   return(
     <ul>
-      {personsToShow.map(person => <Contact key={person.name} name={person.name} number={person.number}/> )}
+      {personsToShow.map(person => 
+        <Contact key={person.name} name={person.name} number={person.number}/>
+      )}
     </ul>
   )
 }
