146d4a9df5d0f2eeb7a961d41ffb59dcda0378cb
Elina Rosato <rosatoelina@gmail.com>
2023-05-15 23:46:32 +0200

    helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,7 +4,14 @@ const dummy = () => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes,0)
 
+const favoriteBlog = (blogs) => {
+  return blogs
+    .map((blog) => ({ title: blog.title, author: blog.author, likes: blog.likes }))
+    .reduce((fav, blog) => fav.likes > blog.likes ? fav : blog , 0)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
