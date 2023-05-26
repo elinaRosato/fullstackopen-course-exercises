2d970658b165732da8690d8c393083f09a33a3aa
Elina Rosato <rosatoelina@gmail.com>
2023-05-26 16:21:29 +0200

    blog list tests, step1

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -11,6 +11,18 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('there are two notes', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].content).toBe('HTML is easy')
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
